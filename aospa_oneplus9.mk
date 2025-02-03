3a1e50fce848bd70bd120a18d7548f8fe3783011
Addy-95 <skabdullah.95@gmail.com>
2025-02-03 12:22:26 +0530

    oneplus9: Add target mk file

---
+++ aospa_oneplus9.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/oneplus/oneplus9/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_BRAND := OnePlus
+PRODUCT_DEVICE := oneplus9
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := LE2115
+PRODUCT_NAME := aospa_oneplus9
+
+PRODUCT_SYSTEM_NAME := OnePlus9
+PRODUCT_SYSTEM_DEVICE := OnePlus9
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus9-user 14 UKQ1.230924.001 R.1a46f9a_1-1 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:14/UKQ1.230924.001/R.1a46f9a_1-1:user/release-keys
